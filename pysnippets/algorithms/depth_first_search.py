19e0ac6c4a76da052b8b1652a81f077da9c46dae
Rishika Hazarika <131283498+rishikaa1@users.noreply.github.com>
2024-10-10 23:35:25 +0530

    Create depth_first_search.py

---
+++ pysnippets/algorithms/depth_first_search.py
@@ -0,0 +1,46 @@
+def dfs(graph, start, visited=None):
+  """
+  Performs Depth First Search (DFS) traversal on a graph.
+
+  Args:
+     graph (dict): The adjacency list representing the graph.
+     start (str): The starting node for DFS traversal.
+     visited (set): Keeps track of visited nodes to avoid cycles (default is None).
+
+  Returns:
+     None: Prints nodes in DFS order during traversal.
+
+  Example:
+     >>> graph = {
+     ... 'A': ['B', 'C'],
+     ... 'B': ['D', 'E'],
+     ... 'C': ['F'],
+     ... 'D': [],
+     ... 'E': ['F'],
+     ... 'F': [],
+     ... }
+     >>> dfs(graph, 'A')
+     A B D E F C
+  """
+  if visited is None:
+    visited = set()
+
+  visited.add(start)
+  print(start, end=' ')
+
+  for neighbor in graph[start]:
+    if neighbor not in visited:
+      dfs(graph, neighbor, visited)
+
+# Example usage
+if __name__ == "__main__":
+  graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+  }
+  dfs(graph, 'A')
+  # Output: A B D E F C
