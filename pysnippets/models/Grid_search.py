786068bf7b082fc516ba0ff6a70584b087e05d76
yashksaini-coder <ys3853428@gmail.com>
2024-10-11 20:55:14 +0530

    Add ML models & technique code snippets

---
+++ pysnippets/models/Grid_search.py
@@ -0,0 +1,15 @@
+import numpy as np
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import LogisticRegression
+
+class GridSearchModel:
+    def __init__(self):
+        self.model = LogisticRegression()
+        self.param_grid = {'C': [0.1, 1, 10]}
+        self.grid_search = GridSearchCV(self.model, self.param_grid)
+
+    def fit(self, X, y):
+        self.grid_search.fit(X, y)
+
+    def best_params(self):
+        return self.grid_search.best_params_
---
+++ pysnippets/models/XGB.py
@@ -0,0 +1,15 @@
+import numpy as np
+import xgboost as xgb
+
+class XGBModel:
+    def __init__(self):
+        self.model = xgb.XGBRegressor()
+
+    def fit(self, X, y):
+        self.model.fit(X, y)
+
+    def predict(self, X):
+        return self.model.predict(X)
+
+    def score(self, X, y):
+        return self.model.score(X, y)
