0b8460f571ba5b1e0955996ae99a76094677bf62
PavanTeja2005 <98730339+PavanTeja2005@users.noreply.github.com>
2024-10-13 11:42:32 +0530

    Added disjoint sets

---
+++ pysnippets/disjoint/disjointsets.py
@@ -0,0 +1,28 @@
+#DisjointSet
+
+class DisjointSet:
+    def __init__(self, n):
+        self.parent = [i for i in range(n)]
+        self.rank = [0] * n
+
+    def find(self, u):
+        if self.parent[u] != u:
+            self.parent[u] = self.find(self.parent[u])  # Path compression
+        return self.parent[u]
+
+    def union(self, u, v):
+        root_u = self.find(u)
+        root_v = self.find(v)
+
+        if root_u != root_v:
+            # Union by rank
+            if self.rank[root_u] > self.rank[root_v]:
+                self.parent[root_v] = root_u
+            elif self.rank[root_u] < self.rank[root_v]:
+                self.parent[root_u] = root_v
+            else:
+                self.parent[root_v] = root_u
+                self.rank[root_u] += 1
+
+    def connected(self, u, v):
+        return self.find(u) == self.find(v)
