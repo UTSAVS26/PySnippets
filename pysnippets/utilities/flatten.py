e1bd1ceb1e7f062236d7d2bd31f84047b9c410fd
Ananya Gupta <145869907+ananyag309@users.noreply.github.com>
2024-10-13 18:08:14 +0530

    Add files via upload

--- pysnippets/utilities/flatten.py
+++ pysnippets/utilities/flatten.py
@@ -1,11 +1,11 @@
-from typing import List, Any
 
-
-def flatten(nested_list: List[Any]) -> List[Any]:
+def flatten_list(nested_list):
     flat_list = []
+    
     for item in nested_list:
         if isinstance(item, list):
-            flat_list.extend(flatten(item))
+            flat_list.extend(flatten_list(item))  # Recursive call for nested lists
         else:
             flat_list.append(item)
+    
     return flat_list
