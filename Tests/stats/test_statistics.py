5d5fcc33ac087136800e630fe0a7759af0195495
SpreadSheets600 <Sohammaity006@gmail.com>
2024-10-21 12:36:52 +0530

    Added Test Cases For Statistics Snippets

--- Tests/stats/test_statistics.py
+++ Tests/stats/test_statistics.py
@@ -2,6 +2,10 @@ import unittest
 from pysnippets.stats.mean_median_mode import mean, median, mode
 from pysnippets.stats.standard_deviation import standard_deviation
 from pysnippets.stats.variance import variance
+from pysnippets.stats.iqr import calculate_iqr
+from pysnippets.stats.skewness import calculate_skewness
+from pysnippets.stats.kurtosis import calculate_kurtosis
+from pysnippets.stats.correlation import calculate_correlation_coefficient
 
 class TestStatistics(unittest.TestCase):
 
@@ -31,6 +35,33 @@ class TestStatistics(unittest.TestCase):
         self.assertEqual(standard_deviation([1, 2, 3, 4], population=False), 1.2909944487358056)
         with self.assertRaises(ValueError):
             standard_deviation([])
+
+
+    def test_iqr(self):
+        data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        expected_iqr = 5.0  # Q3 - Q1 = 8.5 - 3.5
+        self.assertAlmostEqual(calculate_iqr(data), expected_iqr)
+
+    def test_skewness(self):
+        data = [1, 2, 2, 3, 4, 5, 5, 5, 6, 7]
+        expected_skewness = 0.3397  # Expected skewness value
+        self.assertAlmostEqual(calculate_skewness(data), expected_skewness, places=4)
+
+    def test_kurtosis(self):
+        data = [1, 2, 2, 3, 4, 5, 5, 5, 6, 7]
+        expected_kurtosis = -1.211  # Expected kurtosis value
+        self.assertAlmostEqual(calculate_kurtosis(data), expected_kurtosis, places=3)
+
+    def test_correlation_coefficient(self):
+        data1 = [1, 2, 3, 4, 5]
+        data2 = [5, 4, 3, 2, 1]
+        expected_correlation = -1.0  # Perfect negative correlation
+        self.assertAlmostEqual(calculate_correlation_coefficient(data1, data2), expected_correlation)
+
+        data3 = [1, 2, 3, 4, 5]
+        data4 = [1, 2, 3, 4, 5]
+        expected_correlation = 1.0  # Perfect positive correlation
+        self.assertAlmostEqual(calculate_correlation_coefficient(data3, data4), expected_correlation)
 
 if __name__ == '__main__':
     unittest.main()
