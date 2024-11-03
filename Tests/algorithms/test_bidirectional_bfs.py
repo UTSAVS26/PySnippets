c282914dce04bc673df80b8c48222c7279ec3761
Panda8Bamboo <pandasaster@gmail.com>
2024-11-03 21:03:06 +0800

    tests: populate test_bidirectional_bfs with tests

--- Tests/algorithms/test_bidirectional_bfs.py
+++ Tests/algorithms/test_bidirectional_bfs.py
@@ -12,3 +12,32 @@ class TestBidirectionalBFS(unittest.TestCase):
             'F': ['C', 'E', 'G'],
             'G': ['F']
         }
+
+    def test_no_path(self):
+        disconnected_graph = {
+            'A': ['B'],
+            'B': ['A'],
+            'C': ['D'],
+            'D': ['C']
+        }
+        path = bidirectional_search(disconnected_graph, 'A', 'D')
+        self.assertIsNone(path)
+
+    def test_path(self):
+        path = bidirectional_search(self.graph, 'A', 'G')
+        self.assertEqual(path, ['A', 'C', 'F', 'G'])
+    def test_disconnected_graph(self):
+        two_node_graph = {'A': [], 'B': []}
+        path = bidirectional_search(two_node_graph, 'A', 'B')
+        self.assertIsNone(path)
+
+    def test_invalid_node(self): #currently fails
+        path = bidirectional_search(self.graph, 'A', 'Z')
+        self.assertIsNone(path)
+
+    def test_single_node_graph(self):
+        single_node_graph = {'A': []}
+        path = bidirectional_search(single_node_graph, 'A', 'A')
+        self.assertEqual(path, ['A'])
+
+
