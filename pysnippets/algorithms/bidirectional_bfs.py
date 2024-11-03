9ebe231dc64f5af1993de4487b04231604cd67a9
Panda8Bamboo <pandasaster@gmail.com>
2024-11-03 15:57:15 +0800

    feat: add remake paths functions

--- pysnippets/algorithms/bidirectional_bfs.py
+++ pysnippets/algorithms/bidirectional_bfs.py
@@ -6,6 +6,9 @@ def bidirectional_search(graph, start, target):
     visited_start = {start}
     visited_target = {target}
 
+    parents_start = {start: None}
+    parents_target = {target: None}
+
 def bfs(graph, visited, queue, parents, other_visited):
     current_node = queue.popleft()
     for neighbor in graph[current_node]:
@@ -18,4 +21,20 @@ def bfs(graph, visited, queue, parents, other_visited):
                 return neighbor
     return None
 
-def _remake_path():
+def _remake_path_start(parents_start, meeting_node):
+    path_start = []
+    node = meeting_node
+    while node is not None:
+        path_start.append(node)
+        node = parents_start[node]
+    path_start.reverse()
+    return path_start
+
+def _remake_path_target(parents_target, meeting_node):
+    node = meeting_node
+    path_target = []
+    while node is not None:
+        path_target.append(node)
+        node = parents_target[node]
+    return path_target
+
