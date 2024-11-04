6ad3643d0f7f4a4942dfcbdea0ddf22d5e3f21f6
harpreet <20126185@tafe.wa.edu.au>
2024-11-04 12:57:08 +0800

    Add feature(cocktail sort): To Sorting File ,test cases and update the documentation

--- pysnippets/Sorting/sort_dict_list.py
+++ pysnippets/Sorting/sort_dict_list.py
@@ -93,6 +93,28 @@ def insertion_sort(dict_list, key, reverse=False):
         dict_list[j + 1] = key_item
     return dict_list
 
+def cocktail_sort(dict_list, key, reverse=False):
+    """Cocktail sort implementation """
+    n = len(dict_list)
+    start = 0
+    end = n - 1
+    while start < end:
+        new_end = start
+        for i in range(start, end):
+            if (dict_list[i][key] > dict_list[i + 1][key]) != reverse:
+                dict_list[i], dict_list[i + 1] = dict_list[i + 1], dict_list[i]
+                new_end = i
+        end = new_end
+
+        new_start = end
+        for i in range(end - 1, start - 1, -1):
+            if (dict_list[i][key] > dict_list[i + 1][key]) != reverse:
+                dict_list[i], dict_list[i + 1] = dict_list[i + 1], dict_list[i]
+                new_start = i
+        start = new_start
+
+    return dict_list
+
 
 if __name__ == "__main__":
     data = [
